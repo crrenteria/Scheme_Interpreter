8ddfd4a0d9574697a1f983f819b5d11177882123
Katharine Brinker <katharine.brinker@gmail.com>
2014-04-15 19:39:11 -0700

    Added a test file; began development on parse tree

---
+++ Scheme Interpreter/src/InterpreterTest.java
@@ -0,0 +1,19 @@
+import backend.TreeWalker;
+import frontend.*;
+import intermediate.*;
+
+public class InterpreterTest 
+{ 
+	public static void main(String[] args)
+	{
+
+		Parser parser = new Parser();
+		SchemeScanner scanner = new SchemeScanner();
+		TreeWalker walker = new TreeWalker();
+		 
+		//scanner scans
+		//parser parses
+		//intermediate stuff is built
+		//backend navigates tree and hopefully prints the scheme it got in
+	}
+}
